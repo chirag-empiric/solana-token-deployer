0efcb6f9f98b89e31df67fa56395a74f4cb73bfe
Mukesh Rajbhar <mukesh@empiricinfotech.com>
2024-07-31 16:04:10 +0530

    feat: Added file upload controller

--- server/app.ts
+++ server/app.ts
@@ -1,34 +1,18 @@
-import * as fs from 'fs/promises';
+import dotenv from 'dotenv'
+import express from 'express'
+import bodyParser from 'body-parser'
+import fileRoutes from './src/routes/fileRoutes'
 
-async function uploadFile() {
-    try {
-        const filePath = '/home/hp/Solana-tutorials/upload-arweave/sample2.txt';
+dotenv.config()
 
-        console.log(`filePath is`, filePath)
+const app = express()
+const PORT = process.env.PORT || 3000
 
-        // return
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-        const data: string = await fs.readFile(filePath, 'utf8');
+app.use('/api', fileRoutes)
 
-        const response: Response = await fetch('https://api.akord.com/files', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Api-Key': "QizBVNmw5u6BnYV7mNhhqaIPuvn1P2ViMw3sDLVe",
-                'Content-Type': 'text/plain'
-            },
-            body: data
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to upload file: ${response.statusText}`);
-        }
-
-        const responseData = await response.json();
-        console.log('File uploaded successfully:', responseData);
-    } catch (error) {
-        console.error('Error uploading file:', error);
-    }
-}
-
-uploadFile();
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ server/src/controllers/fileController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express'
+import * as fs from 'fs/promises'
+import fetch from 'node-fetch'
+
+export const uploadFile = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const filePath = '/home/hp/Solana-tutorials/upload-arweave/sample2.txt'
+        // const filePath = process.env.FILE_PATH!
+        const data: string = await fs.readFile(filePath, 'utf8');
+        // const data = await fs.readFile(filePath, 'utf8')
+
+        const response = await fetch('https://api.akord.com/files', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Api-Key': process.env.API_KEY!,
+                'Content-Type': 'text/json'
+            },
+            body: data
+        })
+        if (!response.ok) {
+            throw new Error(`Failed to upload file: ${response.statusText}`)
+        }
+        const responseData = await response.json()
+        console.log('File uploaded successfully:', responseData)
+        res.status(200).json(responseData)
+    } catch (err) {
+        console.error('Error uploading file:', err)
+        res.status(500).json({ message: (err as Error).message })
+    }
+};
