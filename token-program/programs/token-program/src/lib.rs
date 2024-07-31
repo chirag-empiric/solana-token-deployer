bf04fba677971969a470c4997d8eabd64907b124
chirag-empiric <chirag@empiricinfotech.com>
2024-07-31 15:09:11 +0530

    feat: initial token setup

---
+++ token-program/programs/token-program/src/lib.rs
@@ -0,0 +1,123 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3,
+        Metadata as Metaplex,
+    },
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
+declare_id!("Go1W5H25JmSignkRTMDxgo5MxWcy8K6ndbnrdsN6WWv4");
+
+#[program]
+pub mod token_program {
+    use super::*;
+
+    pub fn initiate_token(
+        ctx: Context<InitToken>,
+        token_name: String,
+        token_symbol: String,
+        token_uri: String,
+        token_tax: u16,
+    ) -> Result<()> {
+        let token_name_bytes = token_name.as_bytes();
+        let seeds = &[token_name_bytes, &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+        let token_data: DataV2 = DataV2 {
+            name: token_name.clone(),
+            symbol: token_symbol,
+            uri: token_uri,
+            seller_fee_basis_points: token_tax,
+            creators: None,
+            collection: None,
+            uses: None,
+        };
+
+        let metadatactx = CpiContext::new_with_signer(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                payer: ctx.accounts.payer.to_account_info(),
+                update_authority: ctx.accounts.mint.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
+                mint_authority: ctx.accounts.mint.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+            &signer,
+        );
+
+        create_metadata_accounts_v3(metadatactx, token_data, false, true, None)?;
+
+        msg!("Token mint created successfully.");
+        Ok(())
+    }
+
+    pub fn mint_tokens(ctx: Context<MintTokens>, token_name: String, quantity: u64) -> Result<()> {
+        let token_name_bytes = token_name.as_bytes();
+        let seeds = &[token_name_bytes, &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &signer,
+            ),
+            quantity,
+        )?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(token_name: String)]
+pub struct InitToken<'info> {
+    #[account(mut)]
+    /// CHECK: UncheckedAccount
+    pub metadata: UncheckedAccount<'info>,
+    #[account(
+        init,
+        seeds = [token_name.as_bytes()],
+        bump,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metaplex>,
+}
+
+#[derive(Accounts)]
+#[instruction(token_name: String)]
+pub struct MintTokens<'info> {
+    #[account(
+        mut,
+        seeds = [token_name.as_bytes()],
+        bump,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer,
+    )]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
---
+++ token-program/tests/token-program.ts
@@ -0,0 +1,152 @@
+import * as anchor from "@coral-xyz/anchor";
+import { web3 } from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { TokenProgram } from "../target/types/token_program";
+import { assert } from "chai";
+import { BN } from "bn.js";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, SystemProgram, Transaction } from "@solana/web3.js";
+
+describe("TESTING TOKEN CREATION", () => {
+
+  const myKeypair = Keypair.fromSecretKey(Uint8Array.from([129, 66, 188, 237, 45, 153, 77, 40, 148, 67, 90, 123, 31, 246, 178, 113, 131, 162, 186, 180, 212, 186, 105, 143, 10, 56, 188, 63, 211, 56, 161, 38, 74, 45, 234, 236, 131, 169, 174, 25, 123, 33, 209, 201, 63, 95, 93, 40, 231, 90, 225, 62, 157, 204, 122, 52, 93, 138, 199, 40, 34, 136, 104, 129]));
+
+  const myWallet = new anchor.Wallet(myKeypair);
+
+  const rpcConnection = new Connection(clusterApiUrl("devnet"), {
+    commitment: "confirmed"
+  });
+
+  const newProvider = new anchor.AnchorProvider(rpcConnection, myWallet, {
+    commitment: "confirmed"
+  });
+
+  anchor.setProvider(newProvider);
+
+  const program = anchor.workspace.TokenProgram as Program<TokenProgram>;
+
+  const METADATA_SEED = "metadata";
+  const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+
+  const payer = program.provider.publicKey;
+
+  // set the total amount here - 100 Million
+  const mintAmount = 100;
+
+  // set the toke decimals - 0 for example - MAKE SURE IT MATCHES THE ONE IN THE ```lib.rs``` file
+  const tokenDecimals = 0;
+
+  const TOKEN_NAME = "Token 1";
+  const TOKEN_SYMBOL = "TKN1";
+  const TOKEN_URI = "https://arweave.net/Ls26gntZkKqXcx0TLmTyfZqj-vNaBaEuCo7DvYNMFnw";
+  const TOKEN_TAX = 100; // 100 = 1%
+
+  const [mintWithSeed] = web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(TOKEN_NAME),
+    ],
+    program.programId
+  )
+
+  console.log("Mint is: ", mintWithSeed);
+
+  const [metadataAddress] = web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(METADATA_SEED),
+      TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+      mintWithSeed.toBuffer()
+    ],
+    TOKEN_METADATA_PROGRAM_ID
+  );
+
+
+  it("Initialize", async () => {
+
+    const info = await program.provider.connection.getAccountInfo(mintWithSeed);
+    if (info) {
+      return; // Do not attempt to initialize if already initialized
+    }
+    console.log("  Mint not found. Initializing Program...");
+
+    const context = {
+      metadata: metadataAddress,
+      mint: mintWithSeed,
+      payer,
+      rent: web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: web3.SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+    };
+
+    let fundTx = new Transaction();
+    fundTx.add(
+      SystemProgram.transfer(
+        {
+          fromPubkey: myKeypair.publicKey,
+          toPubkey: mintWithSeed,
+          lamports: 0.001 * LAMPORTS_PER_SOL
+        }
+      )
+    )
+
+    await rpcConnection.sendTransaction(fundTx, [myWallet.payer])
+
+
+    const txHash = await program.methods
+      .initiateToken(TOKEN_NAME, TOKEN_SYMBOL, TOKEN_URI, TOKEN_TAX)
+      .accounts(context)
+      .rpc();
+
+    await program.provider.connection.confirmTransaction(txHash, "finalized");
+    console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+    const newInfo = await program.provider.connection.getAccountInfo(mintWithSeed);
+    assert(newInfo, "  Mint should be initialized.");
+  });
+
+  it("mint tokens", async () => {
+    const destination = await anchor.utils.token.associatedAddress({
+      mint: mintWithSeed,
+      owner: payer,
+    });
+
+    let initialBalance: number;
+
+    try {
+      const balance = await program.provider.connection.getTokenAccountBalance(destination);
+      initialBalance = balance.value.uiAmount;
+    } catch {
+      // Token account not yet initiated has 0 balance
+      initialBalance = 0;
+    }
+
+    const context = {
+      mint: mintWithSeed,
+      destination,
+      payer,
+      rent: web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: web3.SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+    };
+
+    const txHash = await program.methods
+      .mintTokens(TOKEN_NAME, new BN(mintAmount * 10 ** tokenDecimals))
+      .accounts(context)
+      .rpc();
+    await program.provider.connection.confirmTransaction(txHash);
+    console.log(`  https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+
+    const postBalance = (
+      await program.provider.connection.getTokenAccountBalance(destination)
+    ).value.uiAmount;
+    assert.equal(
+      initialBalance + mintAmount,
+      postBalance,
+      "Compare balances, it must be equal"
+    );
+  });
+});
+
+
+// https://arweave.net/Xjqaj_rYYQGrsiTk9JRqpguA813w6NGPikcRyA1vAHM
