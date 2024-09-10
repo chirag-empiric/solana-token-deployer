eca2efae17e9cd7c73bf6c18c8fb1370c818b064
Mukesh Rajbhar <mukesh@empiricinfotech.com>
2024-09-10 12:58:33 +0530

    refactor: Removed the swap.controller.ts  file and dex.routes.ts files

--- server/src/routes/dex.routes.ts
+++
@@ -1,8 +0,0 @@
-import Router from 'express'
-import { swapSplTokens } from '../controllers/swap.controller'
-
-const router = Router()
-
-router.post('/swap', swapSplTokens)
-
-export default router
